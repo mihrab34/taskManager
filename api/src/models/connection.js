05125aca1b76885e22700b9695c4f2096e7d916e
Akintola Ramota <akintolarahmah@gmail.com>
2022-06-15 16:44:52 +0000

    Update api endpoint

--- api/src/models/connection.js
+++
@@ -1,9 +0,0 @@
-require("dotenv").config();
-const mongoose = require("mongoose");
-const MONGODB_CLOUD_URI = "mongodb+srv://mira:mirabel@cluster0.uan1b.mongodb.net/taskmanager?retryWrites=true&w=majority";
-mongoose.connect(MONGODB_CLOUD_URI, {});
-
-const db = mongoose.connection;
-db.once("open", () => {
-  console.log("Connected");
-});
